c07fce4e8fa5e2a1d3263f568f7dd9d9e2415b1c
levijn <66329120+levijn@users.noreply.github.com>
2022-05-30 16:40:55 +0200

    plotted data and edited plot script

--- scripts/plot_test.py
+++ scripts/plot_test.py
@@ -12,8 +12,9 @@ def create_plots(data_location, plots_location, test_name, test_format):
     d = 70
     plt.figure(figsize=(w, h), dpi=d)
     fig, ax = plt.subplots()
-    ax.axis([-2, 0, -1.5, 1.5])
-    real_path_data, estimated_path_data, error, error_x, error_y, time = [], [], [], [], [], []
+    ax.axis([-1.5, 1, -1.75, 1.75])
+    # [time, truex, truey, tagx, tagy, amclx, amcly]
+    real_path_data, tag_path_data, amcl_path_data, tag_error, tag_error_x, tag_error_y, time = [], [], [], [], [], [], []
     
     with open(data_location + "/" + test_name + test_format, "r") as f:
         rospy.loginfo(data_location + "/" + test_name + test_format)
@@ -24,66 +25,79 @@ def create_plots(data_location, plots_location, test_name, test_format):
         print(header)
         print(first_values)
         real_path_data.append((mpath.Path.MOVETO, (first_values[1], first_values[2])))
-        estimated_path_data.append((mpath.Path.MOVETO, (first_values[3], first_values[4])))
+        tag_path_data.append((mpath.Path.MOVETO, (first_values[3], first_values[4])))
+        amcl_path_data.append((mpath.Path.MOVETO, (first_values[5], first_values[6])))
         for row in reader:
-            x_error = abs(float(row[1])-float(row[3]))
-            y_error = abs(float(row[2])-float(row[4]))
-            dist_error = np.sqrt(x_error**2 + y_error**2)
-            error_x.append(x_error)
-            error_y.append(y_error)
-            error.append(dist_error)
+            tag_x_error = abs(float(row[1])-float(row[3]))
+            tag_y_error = abs(float(row[2])-float(row[4]))
+            tag_dist_error = np.sqrt(tag_x_error**2 + tag_y_error**2)
+            tag_error_x.append(tag_x_error)
+            tag_error_y.append(tag_y_error)
+            tag_error.append(tag_dist_error)
             time.append(row[0])
             real_path_data.append((mpath.Path.CURVE4, (row[1], row[2])))
-            estimated_path_data.append((mpath.Path.CURVE4, (row[3], row[4])))
-        avg_error = sum(error) / len(error)
+            tag_path_data.append((mpath.Path.CURVE4, (row[3], row[4])))
+            amcl_path_data.append((mpath.Path.CURVE4, (row[5], row[6])))
+        avg_error = sum(tag_error) / len(tag_error)
 
     real_codes, real_verts = zip(*real_path_data)
-    estimated_codes, estimated_verts = zip(*estimated_path_data)
     real_path = mpath.Path(real_verts, real_codes)
-    estimated_path = mpath.Path(estimated_verts, estimated_codes)
-
     real_patch = mpatches.PathPatch(real_path, edgecolor="red", facecolor="none", lw=2)
-    estimated_patch = mpatches.PathPatch(estimated_path, edgecolor="green", facecolor="none", lw=2)
+
+    tag_codes, tag_verts = zip(*tag_path_data)
+    tag_path = mpath.Path(tag_verts, tag_codes)
+    tag_patch = mpatches.PathPatch(tag_path, edgecolor="green", facecolor="none", lw=2)
+
+    amcl_codes, amcl_verts = zip(*amcl_path_data)
+    amcl_path = mpath.Path(amcl_verts, amcl_codes)
+    amcl_patch = mpatches.PathPatch(amcl_path, edgecolor="yellow", facecolor="none", lw=2)
 
     # Paths
-    dir_name = "Paths"
+    dir_name = "paths"
     save_location = os.path.join(plots_location, dir_name)
     try: 
         os.makedirs(save_location)
     except OSError: 
         pass
-    ax.add_patch(real_patch)
-    ax.add_patch(estimated_patch)
+    ax.add_patch(real_patch, label="real path")
+    ax.add_patch(tag_patch, label="tag estimation path")
+    ax.add_patch(amcl_patch, label="amcl estimation path")
+    plt.legend()
+    plt.title("Path estimation")
+    plt.xlabel("x")
+    plt.ylabel("y")
     plt.savefig(save_location  + "/" + test_name + ".png")
 
     # Error in x
-    dir_name = "Error_in_x"
+    dir_name = "tag_error_in_x"
     save_location = os.path.join(plots_location, dir_name)
     try: 
         os.makedirs(save_location)
     except OSError: 
         pass
     plt.figure()
-    plt.plot(time, error_x)
+    plt.plot(time, tag_error_x)
     plt.xlabel('Time')
     plt.ylabel('Error in x')
+    plt.title("Error in x of tag estimation")
     plt.savefig(save_location  + "/" + test_name + ".png")
 
     # Error in y
-    dir_name = "Error_in_y"
+    dir_name = "tag_error_in_y"
     save_location = os.path.join(plots_location, dir_name)
     try: 
         os.makedirs(save_location)
     except OSError: 
         pass
     plt.figure()
-    plt.plot(time, error_y)
+    plt.plot(time, tag_error_y)
     plt.xlabel('Time')
     plt.ylabel('Error in y')
+    plt.title("Error in y of tag estimation")
     plt.savefig(save_location  + "/" + test_name + ".png")
 
     # Error in distance
-    dir_name = "Error_in_distance"
+    dir_name = "tag_error_in_distance"
     save_location = os.path.join(plots_location, dir_name)
     try: 
         os.makedirs(save_location)
@@ -91,9 +105,10 @@ def create_plots(data_location, plots_location, test_name, test_format):
         pass
     plt.figure()
     plt.axhline(y = avg_error, color = 'r', linestyle = '-')
-    plt.plot(time, error)
+    plt.plot(time, tag_error)
     plt.xlabel('Time')
     plt.ylabel('Error in distance')
+    plt.title("Error in global distance of tag estimation")
     plt.savefig(save_location  + "/" + test_name + ".png")
 
 def all_plots(data_location, plots_location, plotformat=".png"):
