eab9a412121ea94190f4e4eb7b1a41522098f164
Daan Scherrenburg <Daanscherrenburg@gmail.com>
2022-05-31 16:51:04 +0200

    Update z_Kleine_testjes.py

--- scripts/oud/z_Kleine_testjes.py
+++ scripts/oud/z_Kleine_testjes.py
@@ -1,9 +1,10 @@
-import os
-import shutil
+# import os
+# import shutil
 
-datafolders = os.listdir("/home/daan/localization_ws/src/apriltag_localization/tests/")
-os.makedirs("/home/daan/localization_ws/src/apriltag_localization/tests/data/")
-for folder in datafolders:
-    shutil.copy("/home/daan/localization_ws/src/apriltag_localization/tests/" + folder + "/" + folder + ".csv", "/home/daan/localization_ws/src/apriltag_localization/tests/data/")
+# datafolders = os.listdir("/home/daan/localization_ws/src/apriltag_localization/tests/")
+# os.makedirs("/home/daan/localization_ws/src/apriltag_localization/tests/data/")
+# for folder in datafolders:
+#     shutil.copy("/home/daan/localization_ws/src/apriltag_localization/tests/" + folder + "/" + folder + ".csv", "/home/daan/localization_ws/src/apriltag_localization/tests/data/")
 
-
+data_name = "test3_buf10_er02_tdiff02"
+print(data_name[9:11])
